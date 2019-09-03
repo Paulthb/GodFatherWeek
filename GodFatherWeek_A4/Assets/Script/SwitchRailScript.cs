b2e40eb80516afa5c892f61f84711bcf321fb962
paul thibault <paulthb1@gmail.com>
2019-09-03 14:30:28 +0200

    add switchRailScript

---
+++ GodFatherWeek_A4/Assets/Script/SwitchRailScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchRailScript : MonoBehaviour {
+
+    [SerializeField]
+    private Collider2D objectCollider;
+
+	// Use this for initialization
+	void Start () {
+        objectCollider = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

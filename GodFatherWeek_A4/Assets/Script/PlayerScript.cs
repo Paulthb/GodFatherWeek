ec3223c502c9d70fe0709fa0e5cf0d1d330a518e
paul thibault <paulthb1@gmail.com>
2019-09-04 17:46:49 +0200

    random fix

--- GodFatherWeek_A4/Assets/Script/PlayerScript.cs
+++ GodFatherWeek_A4/Assets/Script/PlayerScript.cs
@@ -11,11 +11,11 @@ public class PlayerScript : MonoBehaviour {
         SHOOTER
     }
     public ROLE currentRole;
-
     public GameManager.PLAYER_LIST nbPlayer;
+    public SpriteRenderer SpritePlayerBase;
+    public SpriteRenderer SpritePlayerCanon;
 
     private int score;
-
 
 	// Use this for initialization
 	void Start () {
